0661d18ea8ca4ad734ac993147934375cab442c8
Twoje Imię <twojemail@example.com>
2025-04-21 15:17:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const collapse = document.querySelector('.navbar-collapse');
+const navLinks = document.querySelectorAll('.nav-link'); 
+const navBarLink = document.querySelector('.navbar-nav');
+const navBar = document.querySelector('.navbar');
+const toggler = document.querySelector('.navbar-toggler')
+const phone = document.querySelector('.icon')
+const number = document.querySelector('.number')
+const phonee = document.querySelector('.ask-for-price')
+const nav = document.querySelector('.nav');
+
+
+const daRade = () => {
+    // Działa tylko dla mniejszych ekranów (poniżej 768px)
+    if (!navBar.classList.contains('start') && !window.innerWidth < 768) {
+        navBar.classList.add('start');
+        navBarLink.style.display = 'block';
+      
+    } else   {
+        navBar.classList.remove('start');
+        navBarLink.style.display = 'none';   
+    } 
+}
+toggler.addEventListener('click', daRade);
+
+
+
+
+    const updateNavBarOnResize = () => {
+        // Sprawdzamy, czy okno jest większe niż 768px
+        if (window.innerWidth > 768) {
+            // Sprawdzamy obliczony styl display
+            const computedStyle = getComputedStyle(navBarLink);
+    
+            if (computedStyle.display !== 'flex' && window.innerWidth > 768 ) {
+                navBarLink.style.display = 'flex ';
+                navBarLink.style.flexDirection = 'row';
+                navBar.classList.remove('start');
+            }  else if (!window.innerWidth < 768 && !navBar.classList.contains('start') ) {
+                navBarLink.style.display = '';
+                
+            } else {
+                navBarLink.style.display = 'block';
+// 
+            }
+    
+            // Usuwamy klasę 'start', jeśli jest obecna
+        }
+    };
+    
+    // Inicjalizowanie
+    updateNavBarOnResize();
+    
+    // Nasłuchiwanie zmiany rozmiaru okna
+    window.addEventListener('resize', updateNavBarOnResize);
+
+
+    
+AOS.init();
