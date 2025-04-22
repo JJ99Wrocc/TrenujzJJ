3776994259d96cb5f65c5f34e850af560956f9d6
Twoje Imię <twojemail@example.com>
2025-04-22 13:26:26 +0200

    Opis zmian

--- js/script.js
+++ js/script.js
@@ -11,13 +11,9 @@ const clickMap = document.querySelector('.click-and-see-map')
 const map = document.querySelector('.map')
 
 
-const openMap = () =>{
- if(window.getComputedStyle(map).display === 'none'){
-    map.style.display ='block'
- } else {
-    map.style.display = 'none'
- }
-}
+function openMap() {
+    window.open('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2504.9853170163774!2d16.9996871764448!3d51.108731071725394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fc3de721f5af5%3A0xd5212155188f2d45!2sBonzai%20MMA!5e0!3m2!1sen!2spl!4v1745310480931!5m2!1sen!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"');
+  }
 clickMap.addEventListener('click', openMap)
 const daRade = () => {
     
